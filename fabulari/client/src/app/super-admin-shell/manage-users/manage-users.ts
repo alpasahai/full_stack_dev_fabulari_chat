4d5d115bd631147350a1d72015fd3bebf0c74ea8
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-08-24 22:24:21 +1000

    PHASE 1: Angular Skeleton - Setting the stage

---
+++ fabulari/client/src/app/super-admin-shell/manage-users/manage-users.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-manage-users',
+  styleUrl: './manage-users.scss',
+  templateUrl: './manage-users.html',
+})
+export class ManageUsers {}
