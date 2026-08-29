09e1b86dcd945632e63f72bed40b27122e36be68
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-08-29 21:58:54 +1000

    PHASE 1: Quick Fixes

--- fabulari/client/src/app/super-admin-shell/manage-users/manage-users.ts
+++ fabulari/client/src/app/super-admin-shell/manage-users/manage-users.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GroupService } from '../../core/services/group.service';
+import { AuthService } from '../../core/services/auth';
+import { Group } from '../../core/models/group.model';
+
+//see if you can rename to requestsSA for better clarity
 
 @Component({
-  imports: [],
   selector: 'app-manage-users',
-  styleUrl: './manage-users.scss',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './manage-users.html',
+  styleUrl: './manage-users.scss',
 })
-export class ManageUsers {}
+export class ManageUsers implements OnInit {
+  pendingGroups: Group[] = [];
+
+  constructor(private groupService: GroupService, private auth: AuthService) {}
+
+  ngOnInit() { this.loadPending(); }
+
+  loadPending() {
+    this.groupService.getGroups().subscribe(groups => {
+      this.pendingGroups = groups.filter(g => g.status === 'pending');
+    });
+  }
+
+  approve(group: Group) {
+    this.groupService.updateStatus(group.id, 'approved').subscribe(() => this.loadPending());
+  }
+
+  deny(group: Group) {
+    this.groupService.updateStatus(group.id, 'declined').subscribe(() => this.loadPending());
+  }
+
+  logout() { this.auth.logout(); }
+}
