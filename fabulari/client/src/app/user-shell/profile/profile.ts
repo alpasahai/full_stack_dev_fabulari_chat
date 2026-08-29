da704397302fbecffd6dcbd94e7a2bbe539ae5df
alpie_sahai <168612863+alpasahai@users.noreply.github.com>
2026-08-29 21:51:27 +1000

    PHASE 1: Profile Page - Bare minimum - to be built on later

--- fabulari/client/src/app/user-shell/profile/profile.ts
+++ fabulari/client/src/app/user-shell/profile/profile.ts
@@ -1,9 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../core/services/auth';
+import { UserService } from '../../core/services/user.service';
+import { User } from '../../core/models/user.model';
 
 @Component({
-  imports: [],
   selector: 'app-profile',
-  styleUrl: './profile.scss',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './profile.html',
+  styleUrl: './profile.scss',
 })
-export class Profile {}
+export class Profile implements OnInit {
+  user!: User & { email?: string };
+  editing = false;
+  editUsername = '';
+  editEmail = '';
+
+  constructor(private auth: AuthService, private userService: UserService, private router: Router) {}
+
+  ngOnInit() {
+    this.user = this.auth.getCurrentUser()! as any;
+    this.editUsername = this.user.username;
+    this.editEmail = this.user.email || '';
+  }
+
+  //Enabling eddditing
+  startEdit() { this.editing = true; }
+
+  //Cancelling said editting
+  cancelEdit() {
+    this.editing = false;
+    this.editUsername = this.user.username;
+    this.editEmail = this.user.email || '';
+  }
+
+  //Saving Changes
+  save() {
+    this.userService.updateUser(this.user.id, {
+      username: this.editUsername,
+      email: this.editEmail
+    } as any).subscribe(updated => {
+      this.user = { ...this.user, ...updated };
+      this.auth.saveSession(this.user);
+      this.editing = false;
+    });
+  }
+
+  //Retruning to previous page
+  goBack() {
+    if (this.user.role === 'superAdmin') this.router.navigate(['/superadmin/requests']);
+    else this.router.navigate(['/app/dashboard']);
+  }
+}
